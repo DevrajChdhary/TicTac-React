e065a9e40169c16118eff435389b5a4b85c020de
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2024-07-23 11:12:35 +0530

    Init

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card/Card'
+import Grid from './components/Grid/Grid'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Grid  numberofCards={9}/>
+    </>
+  )
+}
+
+export default App
